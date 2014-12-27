a2f45b4198caa1318b9e5368cec1dca830c731da
yiqin <yiqin.mems@gmail.com>
2014-12-27 10:45:23 -0600

    Initial Commit

---
+++ notificationCenter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  notificationCenter
+//
+//  Created by Yi Qin on 12/27/14.
+//  Copyright (c) 2014 Yi Qin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ notificationCenter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  notificationCenter
+//
+//  Created by Yi Qin on 12/27/14.
+//  Copyright (c) 2014 Yi Qin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
